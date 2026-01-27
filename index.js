86488e80580087a838e66a38a962c1395e6ef3d6
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-28 02:42:23 +0530

    status 404 shown

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
+app.use((req,res,next)=>{
+  res.status(404).send(`<h1>Error 404 Page not found</h1>`)
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
