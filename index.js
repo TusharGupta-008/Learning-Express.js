6964e5713c391b7524359fb4c1ae469166dd9291
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-27 05:54:39 +0530

    adding middlewares

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express();
 
 const Port = 3000;
 
+
+app.use((req, res, next) => {
+  console.log("Coming in first middleware", req.url, res.url);
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Coming in Second middleware", req.url, res.url);
+  next();
+});
+
+
 app.listen(Port, () => {
   console.log(`Example app listening on port ${Port}`);
 });
-
