4b53529e1a692d143f2d9a0db91de501ab7fcf19
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-26 18:22:28 +0530

    new file created

--- routes/error.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const errorRouter = express.Router();
-
-errorRouter.use((req, res, next) => {
-  res.status(404).send(`<h1>Error 404 Page not found</h1>`);
-});
-
-module.exports = errorRouter;
