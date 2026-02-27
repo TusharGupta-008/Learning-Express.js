a956103702ac09facd0caba558c24f514b56dd8f
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-27 19:23:24 +0530

    models put in file

---
+++ Models/User.model.js
@@ -0,0 +1,19 @@
+const userModel = {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+};
+module.exports = userModel;
